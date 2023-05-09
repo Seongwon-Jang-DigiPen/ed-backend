ff5c141dba0a3d664abb4d3e26edf1da4d5310d9
seongwon-jang <seongwon.jang@digipen.edu>
2023-05-09 19:29:07 +0900

    cors fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const nunjucks = require("nunjucks");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 // var usersRouter = require('./routes/users');
@@ -19,7 +20,7 @@ nunjucks.configure("views", {
   express: app,
   watch: true,
 });
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
